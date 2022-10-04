8d78fb84cc12d42fd58b28f9e2ca32a4f29dae6a
SumanthhKumarM <113960373+SumanthhKumarM@users.noreply.github.com>
2022-10-04 10:02:56 +0530

    Create odd-cube.c

---
+++ odd-cube.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+int main()
+{
+  	int i, n,z;
+ 
+  	printf("\n Please Enter the Maximum Limit Value : ");
+  	scanf("%d", &n);
+  
+  	printf("\n Even Numbers between 1 and %d are : \n", n);
+  	for(i = 1; i <= n; i++)
+  	{
+    	if ( i % 2 != 0 ) 
+    	{
+  			printf(" %d\t", i*i*i);
+    	}
+}
+return 0;
+}
